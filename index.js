7ebecb5c9790cb9bca7a1d75ae32b1e6fb0d94eb
zzming <zzming98@qq.com>
2015-09-15 17:30:35 +0800

    fix regexp

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 var path = require('path');
-var reg = /(?!https?:)[\w\/\-.]+?\.(?:png|jpg|gif|bmp|jpe|jpeg|gif|ico|zip|rar|webp|swf)/ig;
-
+var reg = /(['"])\s*((?!https?:\/\/)[\w\-\/.]+?\.(?:png|jpg|gif|bmp|jpe|jpeg|webp|swf|ico))\s*\1/ig;
 module.exports = function(str) {
   if(str instanceof Buffer) str = str.toString();
   var ret = [];
-  str.replace(reg, function(m) {
-    m = path.normalize(m.trim());
-    if(!m)  return;
-    ret.push({
-      path: m
-    });
+  str.replace(reg, function(match, quote, imgpath) {
+    imgpath = path.normalize(imgpath.trim());
+    imgpath && ret.push({"path": imgpath});
   });
   return ret;
 }
